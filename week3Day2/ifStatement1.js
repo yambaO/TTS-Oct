d42490edbe780f05e698060e945c51746958af5e
YambaO <yamba.ouandaogo@outlook.com>
2022-11-01 11:02:20 -0500

    Adding homework challenges

---
+++ week3Day2/ifStatement1.js
@@ -0,0 +1,10 @@
+console.log('*********************Challenge 1 **************************');
+console.log('Do You Need More Coffee?');
+
+let numberOfCups = 4;
+if( numberOfCups < 3){
+	console.log("Yes I'll take another cup coffee.");
+}
+else{
+	console.log("I think I'm okay for now.");
+}
---
+++ week3Day2/ifStatement2.js
@@ -0,0 +1,11 @@
+console.log('*********************Challenge 2 **************************');
+
+console.log('Does your car need an oil change?');
+
+let numberOfMiles = 300;
+if(numberOfMiles > 500){
+	console.log('Yes I think I need an oil change.');
+}
+else{
+	console.log('My car doesn\'t need an oil change yet');
+}
