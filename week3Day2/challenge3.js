41890ad75a59739c219f3f6fa5dae8f4aae7d0a3
YambaO <yamba.ouandaogo@outlook.com>
2022-11-18 11:17:26 -0600

    Added a to do list app

---
+++ week3Day2/challenge3.js
@@ -0,0 +1,9 @@
+console.log('*********************challenge 3 **************************');
+for(let i = 99; i >= 0; i--){
+    if(i === 0){
+        console.log("Hey! We need more root beer!");
+    }
+else{
+    console.log(i + " bottles of root beer on the wall");
+}
+}
